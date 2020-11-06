e644a11246b367e9a47ad2941b76fcef27a16779
Jonatan <jonatangrassi22@gmail.com>
2020-11-06 14:08:16 -0300

    MakeFileClienteServerEjercicio3

---
+++ APL3Ejercicio_3/Ejercicio3Cliente.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+int main()
+{
+
+    return 1;
+}
