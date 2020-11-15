28bb94b53b8a395bd557e462beeea946e627e56b
JP <juanpablopumarega@gmail.com>
2020-11-15 18:09:45 -0300

    intentando hacer el ADD

--- APL3Ejercicio_4/interfaz.cpp
+++ APL3Ejercicio_4/interfaz.cpp
@@ -14,8 +14,15 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <list>
 
 using namespace std;
+
+
+typedef struct {
+	string nombreCampo;
+    string valor;
+} claveValor;
 
 void ayuda(){
     //cout << "-------------------------------------------------------" << endl;
@@ -154,15 +161,165 @@ string drop(string tableName) {
 
 }
 
+list<claveValor> ordenarRegistro(string schema, list<claveValor> registro) {
+
+        ifstream MyReadFile(schema);
+        string cadena;
+        claveValor estructura;
+        list<claveValor> schemaDeTabla;
+        list<claveValor>::iterator iteradorDeSchema;
+        list<claveValor>::iterator iteradorDeRegistro;
+        
+        while(getline(MyReadFile,cadena)){
+          estructura.nombreCampo=cadena;
+          schemaDeTabla.push_back(estructura);
+        }
+
+        iteradorDeSchema=schemaDeTabla.begin();
+
+        while( iteradorDeSchema != schemaDeTabla.end()) {
+
+            iteradorDeRegistro=registro.begin();
+
+            while( iteradorDeRegistro != registro.end()) {
+                
+                if(iteradorDeRegistro->nombreCampo == iteradorDeSchema->nombreCampo) {
+                    iteradorDeSchema->valor = iteradorDeRegistro->valor;
+                }
+
+                iteradorDeRegistro++;
+            }
+
+            iteradorDeSchema++;
+        }
+
+    return schemaDeTabla;
+
+}
+
+string agregarRegistroATabla(string tableName, list<claveValor> listaOrdenada, string primaryKey) {
+    
+    ifstream lectura;
+    lectura.open(tableName,ios::in);
+
+    if(lectura.fail()){
+        return "ERROR: No se pudo acceder a la tabla " + tableName + ". Intentelo nuevamente";
+    }
+
+    for (std::string linea; std::getline(lectura, linea); )
+    {
+        std::stringstream registro(linea);
+        std::string dato;
+
+        for (int columna = 0; std::getline(registro, dato, ';'); ++columna)
+        {
+            if(columna == 0 && dato == primaryKey) {
+                lectura.close();
+                return "ERROR: Primary Key duplicada, no se pudo insertar.";
+            }
+        }  
+    }
+
+    lectura.close();
+
+    ofstream escritura;
+    escritura.open(tableName.c_str(),ios::app);
+
+    list<claveValor>::iterator iteradorDeSchema;
+    int n=1;
+
+    cout << "abri el archivo" << endl;
+
+    escritura<<"Holamundoquetal"<<endl;
+
+    //while( iteradorDeSchema != listaOrdenada.end())  {
+    //
+    //    escritura<<iteradorDeSchema->valor;
+    //
+    //    if(listaOrdenada.size() == n) {
+    //        escritura<<endl;
+    //    } else {
+    //        escritura<<";";
+    //    }
+    //
+    //    iteradorDeSchema++;
+    //    n++;
+    //}
+
+    escritura.close();
+
+    return "Registro insertado correctamente";
+}
+
+
+string add(string tableName, list<claveValor> registro) {
+
+    string schema = "./schemas/" + tableName + ".schema";
+    string tabla = "./tablas/" + tableName + ".dat";
+    list<claveValor>::iterator iteradorDeSchema;
+    list<claveValor> listaOrdenada = ordenarRegistro(schema, registro);
+
+    iteradorDeSchema=listaOrdenada.begin();
+
+    while( iteradorDeSchema != listaOrdenada.end())  {
+        cout << "Nombre campo: " << iteradorDeSchema->nombreCampo << endl;    
+        cout << "valor del campo: " << iteradorDeSchema->valor << endl;    
+        iteradorDeSchema++;
+    }
+
+    iteradorDeSchema=listaOrdenada.begin();
+        
+    int n=1;
+
+    string registro;
+
+    while( iteradorDeSchema != listaOrdenada.end())  {
+        cout << iteradorDeSchema->valor;
+        if(listaOrdenada.size() == n) {
+            cout << endl;
+        } else {
+            cout << ";";
+        }
+        iteradorDeSchema++;
+        n++;
+    }
+
+
+    return agregarRegistroATabla(tabla,listaOrdenada,"99");
+
+}
+
+
+
+
+
+
 
 int main(int argc, char *argv[]){
 
     cout << find("producto","nombre_producto","producto3") << endl;
-    cout << "Removiendo registro..." << endl;
-    cout << remove("producto","nombre_producto","producto3") << endl;
-    cout << find("producto","nombre_producto","producto3") << endl;
+    //cout << "Removiendo registro..." << endl;
+    //cout << remove("producto","nombre_producto","producto3") << endl;
+    //cout << find("producto","nombre_producto","producto3") << endl;
     //cout << "DROP de la tabla.." << endl;
     //cout << drop("producto") << endl;
+
+    claveValor hashmap;
+    list<claveValor> registro;
+    hashmap.nombreCampo="precio";
+    hashmap.valor="99999";
+    registro.push_back(hashmap);
+    hashmap.nombreCampo="nombre_producto";
+    hashmap.valor="producto99";
+    registro.push_back(hashmap);
+    hashmap.nombreCampo="descripcion";
+    hashmap.valor="prueba de ADD";
+    registro.push_back(hashmap);
+    hashmap.nombreCampo="id_producto";
+    hashmap.valor="99";
+    registro.push_back(hashmap);
+
+    cout << add("producto",registro) << endl;
 
     return EXIT_SUCCESS;
 }
