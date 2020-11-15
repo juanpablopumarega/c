b6dbb025503e9f27c7b7236a2f82acf856a5b1fa
JP <juanpablopumarega@gmail.com>
2020-11-15 14:57:45 -0300

    Se agrega funcion DROP TABLE y funcion REMOVE REGISTER

--- APL3Ejercicio_4/interfaz.cpp
+++ APL3Ejercicio_4/interfaz.cpp
@@ -27,10 +27,14 @@ void ayuda(){
     //cout << "-------------------------------------------------------" << endl;
 }
 
-string buscarEnTabla(const char * tableName, int nroColumna, string datoABuscar) {
+string buscarEnTabla(string tableName, int nroColumna, string datoABuscar) {
     
     ifstream lectura;
     lectura.open(tableName,ios::in);
+
+    if(lectura.fail()){
+        return "ERROR: No se pudo acceder a la tabla " + (string)tableName + ". Intentelo nuevamente";
+    }
 
     for (std::string linea; std::getline(lectura, linea); )
     {
@@ -44,6 +48,8 @@ string buscarEnTabla(const char * tableName, int nroColumna, string datoABuscar)
             }
         }
     }
+
+    lectura.close();
 
     return "ERROR: Valor " + datoABuscar + " no encontrado en la tabla";
 }
@@ -67,25 +73,96 @@ int obtenerNroColumna(string tableName, string nombreCampo) {
 string find(string tableName, string columnName, string value) {
 
     string schema = "./schemas/" + tableName + ".schema";
-    //string tabla = "./tablas/" + tableName + ".dat";
+    string tabla = "./tablas/" + tableName + ".dat";
     int nroColumna = obtenerNroColumna(schema, columnName);
 
     if(nroColumna < 0)
         return "ERROR: Columna " + columnName + " no encontrada en la tabla";
 
-    return buscarEnTabla("./tablas/producto.dat", nroColumna, value);
+    return buscarEnTabla(tabla, nroColumna, value);
+
+}
+
+//https://www.youtube.com/watch?v=CcP2_7yubV8
+string eliminarRegistroPorclave(string tableName, int nroColumna, string datoABuscar) {
+    
+    ifstream lectura;
+    lectura.open(tableName,ios::in);
+
+    string tablaTemporal = tableName + ".tmp";
+
+    ofstream temporal;
+    temporal.open(tablaTemporal.c_str(),ios::out);
+
+    if(lectura.fail()){
+        return "ERROR: No se pudo acceder a la tabla " + tableName + ". Intentelo nuevamente";
+    }
+
+    for (std::string linea; std::getline(lectura, linea); )
+    {
+        std::stringstream registro(linea);
+        std::string dato;
+
+        int flagDeEscritura = 1;
+
+        for (int columna = 0; std::getline(registro, dato, ';'); ++columna)
+        {
+            if(columna == nroColumna && dato == datoABuscar) {
+               flagDeEscritura=0;
+            }
+        }
+
+        if(flagDeEscritura) {
+            temporal<<linea<<endl;
+        }
+            
+    }
+
+    lectura.close();
+    temporal.close();
+
+    remove(tableName.c_str());
+    rename(tablaTemporal.c_str(),tableName.c_str());
+
+    return "Registro eliminado de la tabla correctamente..";
+}
+
+string remove(string tableName, string columnName, string value) {
+
+    string schema = "./schemas/" + tableName + ".schema";
+    string tabla = "./tablas/" + tableName + ".dat";
+
+    int nroColumna = obtenerNroColumna(schema, columnName);
+
+    if(nroColumna < 0)
+        return "ERROR: Columna " + columnName + " no encontrada en la tabla";
+
+    return eliminarRegistroPorclave(tabla, nroColumna, value);
+
+}
+
+string drop(string tableName) {
+
+    string schema = "./schemas/" + tableName + ".schema";
+    string tabla = "./tablas/" + tableName + ".dat";
+
+    if(!remove(tabla.c_str()) && !remove(schema.c_str())) {
+        return "La tabla " + tabla + " removida exitosamente.";
+    } else {
+        return "La tabla " + tabla + " NO se ha podido remover exitosamente.";
+    }
 
 }
 
 
-
 int main(int argc, char *argv[]){
 
-    //int nroColumna = obtenerNroColumna("./schemas/producto.schema", "precio");
-    //string resultado = buscarEnTabla("./tablas/producto.dat",nroColumna,"400");
-
-    string resultado = find("producto","nombre_producto2","producto4");
-    cout << resultado << endl;
+    cout << find("producto","nombre_producto","producto3") << endl;
+    cout << "Removiendo registro..." << endl;
+    cout << remove("producto","nombre_producto","producto3") << endl;
+    cout << find("producto","nombre_producto","producto3") << endl;
+    //cout << "DROP de la tabla.." << endl;
+    //cout << drop("producto") << endl;
 
     return EXIT_SUCCESS;
 }
